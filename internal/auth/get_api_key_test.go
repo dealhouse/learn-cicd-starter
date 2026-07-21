cd72eede36b52731cb4d85e7964d1b533893889a
DeJuan Deal <de.juan1@live.com>
2026-07-21 17:28:47 -0400

    testing CI with failing tests

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"testing"
+	// "errors"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	got, err := GetAPIKey(map[string][]string{"Authorization": {"ApiKey 123"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := "1234"
+	if got != want {
+		t.Fatalf("got %q, want %q", got, want)
+	}
+}
+
+func TestGetAuthHeaderMissingError(t *testing.T) {
+	_, err := GetAPIKey(map[string][]string{})
+	if err.Error() != "no authorization header included" {
+		t.Fatalf("got %q, want %q", err, "no authorization header included")
+	}
+}
+
+func TestGetAPIKeyL2Error(t *testing.T) {
+	_, err := GetAPIKey(map[string][]string{"Authorization": {"Bearer 1"}})
+	if err.Error() != "malformed authorization header" {
+		t.Fatalf("got %q, want %q", err, "malformed authorization header")
+	}
+}
+
+func TestGetAPIKeyNoKeyError(t *testing.T) {
+	_, err := GetAPIKey(map[string][]string{"Authorization": {"1234"}})
+	if err.Error() != "malformed authorization header" {
+		t.Fatalf("got %q, want %q", err, "malformed authorization header")
+	}
+}
